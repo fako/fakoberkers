3526cf972a480347a80c2311f2767bbde383d1d9
Fako <email@fakoberkers.nl>
2013-07-01 20:50:52 +0200

    Slight improvements to config

--- src/fakoberkers/settings.py
+++ src/fakoberkers/settings.py
@@ -52,7 +52,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = SERVER_ROOT+'media/'
+MEDIA_ROOT = SERVER_ROOT+'media/' + PROJECT_NAME + '/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -63,7 +63,7 @@ MEDIA_URL = '/media/' + PROJECT_NAME + '/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = SERVER_ROOT+'static/'
+STATIC_ROOT = SERVER_ROOT + 'static/' + PROJECT_NAME + '/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
