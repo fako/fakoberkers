17d973f6236cfadee3421e317ce71a891f2237bf
Fako <email@fakoberkers.nl>
2014-03-22 14:55:27 +0100

    Improves update script to run in cron.

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-find ../ -name "*.pyc" | xargs rm;
+project_directory = /srv/django1.6/fakoberkers/
+
+find $project_directory -name "*.pyc" | xargs rm;
 sudo service uwsgi restart;
 sudo /etc/init.d/celeryd restart;
