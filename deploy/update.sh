af9f70ac565ad2387e8e9070067ac42a93fb9ba4
Fako <email@fakoberkers.nl>
2014-03-22 14:59:07 +0100

    Updates the update script again.

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-project_directory = /srv/django1.6/fakoberkers/
+PROJECT_DIR=/srv/django1.6/fakoberkers/
 
-find $project_directory -name "*.pyc" | xargs rm;
+find $PROJECT_DIR -name "*.pyc" | xargs rm;
 sudo service uwsgi restart;
 sudo /etc/init.d/celeryd restart;
