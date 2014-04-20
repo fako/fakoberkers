3e0781c39176e396aa347807836b09345e47cb44
fako <email@fakoberkers.nl>
2014-04-20 14:09:27 +0200

    Update HIF repo, improve update script and allow for log symlinks.

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Start update"
+
 PROJECT_DIR=/srv/django1.6/fakoberkers/
 
 find $PROJECT_DIR -name "*.pyc" | xargs rm;
 sudo service uwsgi restart;
 sudo /etc/init.d/celeryd restart;
+
+echo "End update"
