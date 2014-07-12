2a969828bb7875d1593a44ae36690474a0857a52
fako <email@fakoberkers.nl>
2014-07-12 11:58:22 +0200

    Restructuring template structure.

--- src/digitasenscia/views.py
+++ src/digitasenscia/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, RequestContext
 
 def home(request):
-    return render_to_response('digita-senscia.html', {}, RequestContext(request))
+    return render_to_response('digitasenscia/digita-senscia.html', {}, RequestContext(request))
 
 def partials(request, partial):
-    return render_to_response(partial, {}, RequestContext(request))
+    return render_to_response('digitasenscia/'+partial, {}, RequestContext(request))
