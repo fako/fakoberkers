acf1d4cc70d9a754d0d8f3062e3c076405e258e3
fako <email@fakoberkers.nl>
2014-07-12 12:19:44 +0200

    Make inline settings work better.

--- src/globescope/views.py
+++ src/globescope/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render_to_response, RequestContext
 
 def home(request):
-    return render_to_response('globescope/globe-scope.html', {}, RequestContext(request))
+
+    context = """
+        $window = $(window);
+        window.HIF = {
+            resultSettings: {
+                ratioWidth: 16,
+                ratioHeight: 9,
+                containerWidth: $window.width(),
+                containerHeight: $window.height(),
+                minimalWidth: 800,
+                minimalHeight: 450,
+                animationInterval: 10
+            },
+            waitingSettings: {
+                retry_speed: 1000,
+                refresh_speed: 250,
+                refresh_rate: 0.25,
+                expected_duration: 5000,
+                max_progress: 100
+            },
+            translationEndpoint: "http://{host}/en/hif/image-translations/",
+            questionEndpoint: "http://{host}/en/question/"
+        };
+    """.replace('{host}', request.get_host())
+
+    return render_to_response('globescope/globe-scope.html', {'HIF_FRONTEND_SETTINGS': context}, RequestContext(request))
 
 def partials(request, partial):
     return render_to_response('globescope/'+partial, {}, RequestContext(request))
